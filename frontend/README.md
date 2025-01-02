a96f83286078e37fa126f14e65c4294440a10083
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2025-01-02 10:40:16 +0000

    frontend only

---
+++ frontend/README.md
@@ -0,0 +1,42 @@
+<!-- Hero Section -->
+
+I'm Carlston Rebelo
+
+I'm Carlston A Web Developer & 3D Modeller
+
+<!-- Resume Section -->
+
+View My CV
+
+<!-- Showcase Section -->
+
+3D Modelling
+I know how to 3D model.
+I learn't Autodesk Maya in University and I am now using blender
+
+Web Development
+I am learning Web Development through a web development course
+
+<!-- About Me Section -->
+
+I am a friendly person with a passion in the design industry, I love to 3D model and I am really enjoying making and desiging websites. Apart from this industry I have a few hobbies like:
+Tennis
+Badminton
+Cycling
+Football
+Video games
+I have also have Interests in:
+Socialising with friends and family
+Extra-curricular
+Volunteer at the Church of St. Michael and Martin in Hounslow
+
+<!-- Contact Me -->
+
+Email : crebel325@gmail.com
+LinkedIn
+Sketchfab
+Artstation
+
+<!-- Footer -->
+
+©carlston rebelo
---
+++ frontend/index.js
@@ -0,0 +1,166 @@
+//Button hover effect
+const numberOfButtons = document.querySelectorAll(".btn").length;
+for (var i = 0; i < numberOfButtons; i++) {
+  document
+    .querySelectorAll(".btn")
+    [i].addEventListener("mouseover", function () {
+      this.classList.add("btnHighlight");
+    });
+
+  document
+    .querySelectorAll(".btn")
+    [i].addEventListener("mouseout", function () {
+      this.classList.remove("btnHighlight");
+    });
+}
+// Hamburger Menu Dropdown
+const hamburgerSVG = document.querySelector(".hamburger");
+const dropdownList = document.querySelector(".dropdown-list");
+
+hamburgerSVG.addEventListener("mouseenter", () => {
+  dropdownList.style.display = "block";
+});
+hamburgerSVG.addEventListener("mouseleave", () => {
+  setTimeout(() => {
+    if (!dropdownList.matches(":hover")) {
+      dropdownList.style.display = "none";
+    }
+  }, 100); // Small delay to allow hovering over dropdown before hiding it
+});
+
+dropdownList.addEventListener("mouseenter", () => {
+  dropdownList.style.display = "block";
+});
+dropdownList.addEventListener("mouseleave", () => {
+  setTimeout(() => {
+    if (!dropdownList.matches(":hover")) {
+      dropdownList.style.display = "none";
+    }
+  }, 100); // Delay allows for smoother transitions
+});
+// CV Preview & Download
+const cvDropdownBTN = document.getElementById("cv-btn");
+const cvDropdownDiv = document.getElementById("cv-dropdown");
+cvDropdownBTN.addEventListener("click", () => {
+  cvDropdownDiv.classList.toggle("show");
+});
+cvDropdownDiv.addEventListener("mouseover", () => {
+  cvDropdownDiv.classList.add("show");
+});
+cvDropdownDiv.addEventListener("mouseout", () => {
+  cvDropdownDiv.classList.remove("show");
+});
+
+const cvPreviewBTN = document.getElementById("cv-preview");
+const previewFileDiv = document.getElementById("preview-file-div");
+const cvPreviewFrame = document.getElementById("cv-file");
+const closePreview = document.getElementById("close-preview");
+
+cvPreviewBTN.addEventListener("click", function () {
+  previewFileDiv.classList.add("display-preview");
+});
+closePreview.addEventListener("click", function () {
+  previewFileDiv.classList.remove("display-preview");
+});
+// Project showcase
+fetch("http://127.0.0.1:5000/projects")
+  .then(async (response) => {
+    const result = await response.json();
+
+    const mapShowcases = result.map((showcase) => {
+      if (
+        showcase.githubLink === undefined &&
+        showcase.websiteLink === undefined
+      ) {
+        return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.link}>Visit</a>
+              </button>
+            </div>
+          </div>
+        `;
+      } else if (showcase.websiteLink === undefined) {
+        return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.githubLink}>Github</a>
+              </button>
+            </div>
+          </div>
+        `;
+      } else if (showcase.githubLink === undefined) {
+        return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.websiteLink}>Live</a>
+              </button>
+            </div>
+          </div>
+        `;
+      } else {
+        return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.websiteLink}>Live</a>
+              </button>
+              <button class="btn">
+                <a href=${showcase.githubLink}>Github</a>
+              </button>
+            </div>
+          </div>
+        `;
+      }
+    });
+    mapShowcases.reverse();
+    document.getElementById("showcase-container").innerHTML =
+      mapShowcases.join("");
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+  });
+// Profile image carousel
+fetch("http://127.0.0.1:5000/profile_image")
+  .then((response) => response.json())
+  .then((result) => {
+    const profileImagesArray = result;
+    const profileImage = document.getElementById("profile-image");
+    const profileImageMobile = document.getElementById("profile-image-mobile");
+
+    let currentIndex = 0;
+    setInterval(() => {
+      // Increment index and reset if it exceeds the array length
+      currentIndex = (currentIndex + 1) % profileImagesArray.length;
+
+      // Update the image source
+      profileImage.src = profileImagesArray[currentIndex];
+      profileImageMobile.src = profileImagesArray[currentIndex];
+    }, 6000);
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+  });

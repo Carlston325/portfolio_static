51f64f59b039bc9646d87dacd35fa97c39ada481
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2025-01-02 14:55:28 +0000

    corrected js layout, btn hover, cv preview

--- index.js
+++ index.js
@@ -1,152 +1,160 @@
-//Button hover effect
-const numberOfButtons = document.querySelectorAll(".btn").length;
-for (var i = 0; i < numberOfButtons; i++) {
-  document
-    .querySelectorAll(".btn")
-    [i].addEventListener("mouseover", function () {
-      this.classList.add("btnHighlight");
+window.onload = async function () {
+  // Project showcase
+  await fetch("http://127.0.0.1:5000/projects")
+    .then(async (response) => {
+      const result = await response.json();
+
+      const mapShowcases = result.map((showcase) => {
+        if (showcase.github_link === null && showcase.website_link === null) {
+          return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.other_link}>Visit</a>
+              </button>
+            </div>
+          </div>
+        `;
+        } else if (showcase.website_link === null) {
+          return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.github_link}>Github</a>
+              </button>
+            </div>
+          </div>
+        `;
+        } else if (showcase.github_link === null) {
+          return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.website_link}>Live</a>
+              </button>
+            </div>
+          </div>
+        `;
+        } else {
+          return `
+          <div class="showcase-card">
+            <img class="showcase-img" src=${showcase.thumbnail} />
+            <div>
+              <h2>${showcase.title}</h2>
+              <p>${showcase.descrip}</p>
+            </div>
+            <div>
+              <button class="btn">
+                <a href=${showcase.website_link}>Live</a>
+              </button>
+              <button class="btn">
+                <a href=${showcase.github_link}>Github</a>
+              </button>
+            </div>
+          </div>
+        `;
+        }
+      });
+      mapShowcases.reverse();
+      document.getElementById("showcase-container").innerHTML =
+        mapShowcases.join("");
+    })
+    .catch((error) => {
+      console.error(`Error: ${error.message}`);
     });
+  // Profile image carousel
+  await fetch("http://127.0.0.1:5000/profile_image")
+    .then((response) => response.json())
+    .then((result) => {
+      const profileImagesArray = result;
+      const profileImage = document.getElementById("profile-image");
+      const profileImageMobile = document.getElementById(
+        "profile-image-mobile"
+      );
 
-  document
-    .querySelectorAll(".btn")
-    [i].addEventListener("mouseout", function () {
-      this.classList.remove("btnHighlight");
+      let currentIndex = 0;
+      setInterval(() => {
+        // Increment index and reset if it exceeds the array length
+        currentIndex = (currentIndex + 1) % profileImagesArray.length;
+
+        // Update the image source
+        profileImage.src = profileImagesArray[currentIndex];
+        profileImageMobile.src = profileImagesArray[currentIndex];
+      }, 6000);
+    })
+    .catch((error) => {
+      console.error(`Error: ${error.message}`);
     });
-}
-// Hamburger Menu Dropdown
-const hamburgerSVG = document.querySelector(".hamburger");
-const dropdownList = document.querySelector(".dropdown-list");
-const closeMenuBtn = document.querySelector(".close-menu-btn");
+  //Button hover effect
+  const numberOfButtons = document.querySelectorAll(".btn").length;
+  console.log(numberOfButtons);
 
-hamburgerSVG.addEventListener("click", () => {
-  dropdownList.style.display = "block";
-});
-closeMenuBtn.addEventListener("click", () => {
-  dropdownList.style.display = "none";
-});
-// CV Preview & Download
-const cvDropdownBTN = document.getElementById("cv-btn");
-const cvDropdownDiv = document.getElementById("cv-dropdown");
-cvDropdownBTN.addEventListener("click", () => {
-  cvDropdownDiv.classList.toggle("show");
-});
-cvDropdownDiv.addEventListener("mouseover", () => {
-  cvDropdownDiv.classList.add("show");
-});
-cvDropdownDiv.addEventListener("mouseout", () => {
-  cvDropdownDiv.classList.remove("show");
-});
+  for (let i = 0; i < numberOfButtons; i++) {
+    document
+      .querySelectorAll(".btn")
+      [i].addEventListener("mouseover", function () {
+        this.classList.add("btnHighlight");
+      });
+    document
+      .querySelectorAll(".btn")
+      [i].addEventListener("mouseout", function () {
+        this.classList.remove("btnHighlight");
+      });
+  }
+  // Hamburger Menu Dropdown
+  const hamburgerSVG = document.querySelector(".hamburger");
+  const dropdownList = document.querySelector(".dropdown-list");
+  const closeMenuBtn = document.querySelector(".close-menu-btn");
 
-const cvPreviewBTN = document.getElementById("cv-preview");
-const previewFileDiv = document.getElementById("preview-file-div");
-const cvPreviewFrame = document.getElementById("cv-file");
-const closePreview = document.getElementById("close-preview");
-
-cvPreviewBTN.addEventListener("click", function () {
-  previewFileDiv.classList.add("display-preview");
-});
-closePreview.addEventListener("click", function () {
-  previewFileDiv.classList.remove("display-preview");
-});
-// Project showcase
-fetch("http://127.0.0.1:5000/projects")
-  .then(async (response) => {
-    const result = await response.json();
-
-    const mapShowcases = result.map((showcase) => {
-      if (
-        showcase.githubLink === undefined &&
-        showcase.websiteLink === undefined
-      ) {
-        return `
-          <div class="showcase-card">
-            <img class="showcase-img" src=${showcase.thumbnail} />
-            <div>
-              <h2>${showcase.title}</h2>
-              <p>${showcase.descrip}</p>
-            </div>
-            <div>
-              <button class="btn">
-                <a href=${showcase.link}>Visit</a>
-              </button>
-            </div>
-          </div>
-        `;
-      } else if (showcase.websiteLink === undefined) {
-        return `
-          <div class="showcase-card">
-            <img class="showcase-img" src=${showcase.thumbnail} />
-            <div>
-              <h2>${showcase.title}</h2>
-              <p>${showcase.descrip}</p>
-            </div>
-            <div>
-              <button class="btn">
-                <a href=${showcase.githubLink}>Github</a>
-              </button>
-            </div>
-          </div>
-        `;
-      } else if (showcase.githubLink === undefined) {
-        return `
-          <div class="showcase-card">
-            <img class="showcase-img" src=${showcase.thumbnail} />
-            <div>
-              <h2>${showcase.title}</h2>
-              <p>${showcase.descrip}</p>
-            </div>
-            <div>
-              <button class="btn">
-                <a href=${showcase.websiteLink}>Live</a>
-              </button>
-            </div>
-          </div>
-        `;
-      } else {
-        return `
-          <div class="showcase-card">
-            <img class="showcase-img" src=${showcase.thumbnail} />
-            <div>
-              <h2>${showcase.title}</h2>
-              <p>${showcase.descrip}</p>
-            </div>
-            <div>
-              <button class="btn">
-                <a href=${showcase.websiteLink}>Live</a>
-              </button>
-              <button class="btn">
-                <a href=${showcase.githubLink}>Github</a>
-              </button>
-            </div>
-          </div>
-        `;
-      }
-    });
-    mapShowcases.reverse();
-    document.getElementById("showcase-container").innerHTML =
-      mapShowcases.join("");
-  })
-  .catch((error) => {
-    console.error(`Error: ${error.message}`);
+  hamburgerSVG.addEventListener("click", () => {
+    dropdownList.style.display = "block";
   });
-// Profile image carousel
-fetch("http://127.0.0.1:5000/profile_image")
-  .then((response) => response.json())
-  .then((result) => {
-    const profileImagesArray = result;
-    const profileImage = document.getElementById("profile-image");
-    const profileImageMobile = document.getElementById("profile-image-mobile");
-
-    let currentIndex = 0;
-    setInterval(() => {
-      // Increment index and reset if it exceeds the array length
-      currentIndex = (currentIndex + 1) % profileImagesArray.length;
-
-      // Update the image source
-      profileImage.src = profileImagesArray[currentIndex];
-      profileImageMobile.src = profileImagesArray[currentIndex];
-    }, 6000);
-  })
-  .catch((error) => {
-    console.error(`Error: ${error.message}`);
+  closeMenuBtn.addEventListener("click", () => {
+    dropdownList.style.display = "none";
   });
+  const numberOfMenuOptions = document.querySelectorAll(".btn2").length;
+  for (let i = 0; i < numberOfMenuOptions; i++) {
+    document.querySelectorAll(".btn2")[i].addEventListener("click", () => {
+      dropdownList.style.display = "none";
+    });
+  }
+  // CV Preview & Download
+  const cvDropdownBTN = document.getElementById("cv-btn");
+  const cvDropdownDiv = document.getElementById("cv-dropdown");
+  cvDropdownBTN.addEventListener("click", () => {
+    cvDropdownDiv.classList.toggle("show");
+  });
+  cvDropdownDiv.addEventListener("mouseover", () => {
+    cvDropdownDiv.classList.add("show");
+  });
+  cvDropdownDiv.addEventListener("mouseout", () => {
+    cvDropdownDiv.classList.remove("show");
+  });
+
+  const cvPreviewBTN = document.getElementById("cv-preview");
+  const previewFileDiv = document.getElementById("preview-file-div");
+  const cvPreviewFrame = document.getElementById("cv-file");
+  const closePreview = document.getElementById("close-preview");
+
+  cvPreviewBTN.addEventListener("click", function () {
+    previewFileDiv.classList.add("display-preview");
+  });
+  closePreview.addEventListener("click", function () {
+    previewFileDiv.classList.remove("display-preview");
+  });
+};

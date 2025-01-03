7206a2818bf1205daa21df15c938de7163f63a28
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2025-01-03 14:06:26 +0000

    db updated for backend repo

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,15 @@
-import pg from "pg";
-import dotenv from "dotenv";
+export const profileImageSources = [
+  "./assets/images/Profile/carlston_1.png",
+  "./assets/images/Profile/carlston_2.png",
+  "./assets/images/Profile/carlston_3.png",
+  "./assets/images/Profile/carlston_4.png",
+];
 
-const { Pool } = pg;
+import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
+const { Pool } = pg;
 
 const user = process.env.DB_USER;
 const host = process.env.DB_HOST;
@@ -12,18 +18,18 @@ const password = process.env.DB_PASS;
 const port = process.env.DB_PORT;
 
 const pool = new Pool({
-  user: user,
-  host: host,
-  database: database,
-  password: password,
-  port: port,
+  connectionString: `postgres://${user}:${password}@${host}:${port}/${database}`,
+  ssl: {
+    rejectUnauthorized: false, // For Render's SSL requirements
+  },
 });
 
-export const profileImageSources = [
-  "./assets/images/Profile/carlston_1.png",
-  "./assets/images/Profile/carlston_2.png",
-  "./assets/images/Profile/carlston_3.png",
-  "./assets/images/Profile/carlston_4.png",
-];
-
 export default pool;
+
+// const pool = new Pool({
+//   user: user,
+//   host: host,
+//   database: database,
+//   password: password,
+//   port: port,
+// });
